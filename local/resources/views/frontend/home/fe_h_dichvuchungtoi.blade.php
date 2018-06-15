a39dab0138c90e4b665003744b683b262e9dc940
nnduyquang@gmail.com <bunvodich>
2018-06-15 10:47:31 +0700

    ok

--- local/resources/views/frontend/home/fe_h_dichvuchungtoi.blade.php
+++ local/resources/views/frontend/home/fe_h_dichvuchungtoi.blade.php
@@ -1,46 +1,20 @@
-
 <div class="container-fluid mt-5 mb-4">
     <div class="container">
         <div class="row">
-            <div class="col-md-4 mb-4">
-                <div class="fe-h-dvchungtoi">
-                    <img src="images/uploads/service-5.png" alt="" style="width: 100%;height: auto;">
-                    <div class="fe-h-dvchungtoi-hover">
-                        <div class="d-flex align-items-center justify-content-center pt-3">
-                            <i class="fas fa-screwdriver fa-2x"></i> <span>DỊCH VỤ SỬA CHỬA</span>
+            @foreach($data['services'] as $key=>$item)
+                <div class="col-md-4 mb-4">
+                    <div class="fe-h-dvchungtoi">
+                        {{ Html::image($item->image,'',array('class'=>'')) }}
+                        <div class="fe-h-dvchungtoi-hover">
+                            <div class="d-flex align-items-center justify-content-center pt-3">
+                                <i class="fas fa-screwdriver fa-2x"></i> <span>{{$item->title}}</span>
+                            </div>
+                            <p class="p-4">{{$item->description}}</p>
+                            <a href="{{URL::to('dich-vu/'.$item->path)}}">Xem Thêm</a>
                         </div>
-                        <p class="p-4">Resize the browser window to see how the font size scales.</p>
-                        <a href="">Read more.!</a>
                     </div>
                 </div>
-            </div>
-
-            <div class="col-md-4 mb-4">
-                <div class="fe-h-dvchungtoi">
-                    <img src="images/uploads/service-1.jpg" alt="" style="width: 100%;height: auto;">
-                    <div class="fe-h-dvchungtoi-hover">
-                        <div class="d-flex align-items-center justify-content-center pt-3">
-                            <i class="fas fa-toolbox fa-2x"></i> <span>THAY ẮC QUY GIÁ TỐT</span>
-                        </div>
-                        <p class="p-4">Resize the browser window to see how the font size scales.</p>
-                        <a href="">Read more.!
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 mb-4">
-                <div class="fe-h-dvchungtoi">
-                    <img src="images/uploads/service-8.png" alt="" style="width: 100%;height: auto;">
-                    <div class="fe-h-dvchungtoi-hover">
-                        <div class="d-flex align-items-center justify-content-center pt-3">
-                            <i class="fas fa-wrench fa-2x"></i> <span>DỊCH VỤ KIỂM TRA BẢO TRÌ</span>
-                        </div>
-                        <p class="p-4">Resize the browser window to see how the font size scales.</p>
-
-                        <a href="">Read more.!</a>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </div>
